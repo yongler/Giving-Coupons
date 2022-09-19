e2e960e996087242a256165678abca62abc7292d
Stuart Long Chay Boon <chayboon@gmail.com>
2022-09-20 02:22:30 +0800

    Create admin page for individual campaigns

---
+++ pages/admin/campaigns/[id].js
@@ -0,0 +1,105 @@
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import styles from "../../../styles/Admin.campaigns.page.module.css";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+import ListItemText from "@mui/material/ListItemText";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+
+export default function Campaign({ data }) {
+  const campaign = data;
+
+  console.log(campaign);
+  const charityMappings = {};
+  for (let i = 0; i < campaign.charitiesChosenByDonor.length; i++) {
+    charityMappings[campaign.charitiesChosenByDonor[i].id] =
+      campaign.charitiesChosenByDonor[i].name;
+  }
+
+  return (
+    <div className={styles.formpage}>
+      <Paper className={styles.form} elevation={0}>
+        <Typography gutterBottom variant="h5" component="div">
+          {`Campaign Name: ${campaign.name}`}
+        </Typography>
+        <Typography gutterBottom variant="h5" component="div">
+          {`Donor: ${campaign.donor}`}
+        </Typography>
+        <Typography gutterBottom variant="h5" component="div">
+          {`${campaign.endDate} left`}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {`Description: ${campaign.description}`}
+        </Typography>
+        <Typography className={styles.title} variant="h6" component="div">
+          Charities Selected
+        </Typography>
+        <List dense={true}>
+          {campaign.charitiesChosenByDonor.map((charity) => (
+            <ListItem
+              key={charity.id}
+              secondaryAction={
+                <IconButton edge="end" aria-label="delete">
+                  <DeleteIcon />
+                </IconButton>
+              }
+            >
+              <ListItemText primary={charity.name} />
+            </ListItem>
+          ))}
+        </List>
+        <Typography className={styles.title} variant="h6" component="div">
+          Vouchers Status
+        </Typography>
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>ID</TableCell>
+              <TableCell align="right">Status</TableCell>
+              <TableCell align="right">Charity Selected</TableCell>
+              <TableCell align="right">Amount Added</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {campaign.vouchers.map((voucher) => (
+              <TableRow
+                key={voucher.id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {voucher.id}
+                </TableCell>
+                <TableCell align="right">
+                  {voucher.status == 0
+                    ? "Unredeemed"
+                    : voucher.status == 1
+                    ? "Redeemed"
+                    : "Expired"}
+                </TableCell>
+                <TableCell align="right">
+                  {voucher.charityId ? charityMappings[voucher.charityId] : "-"}
+                </TableCell>
+                <TableCell align="right">
+                  {voucher.amountAdded ? "$" + voucher.amountAdded : "$0"}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Paper>
+    </div>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const id = context.params.id;
+  const res = await fetch(process.env.URL + `/api/campaigns/` + id);
+  const data = await res.json();
+  return { props: { data } };
+}
