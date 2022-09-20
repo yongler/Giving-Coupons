a92da1690763ce37a1e1b22879ba6d3f3ad719ca
Stuart Long Chay Boon <chayboon@gmail.com>
2022-09-21 01:02:16 +0800

    Add box around tables

--- pages/admin/campaigns/[id].js
+++ pages/admin/campaigns/[id].js
@@ -3,14 +3,13 @@ import Typography from "@mui/material/Typography";
 import styles from "../../../styles/Admin.campaigns.page.module.css";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
-import IconButton from "@mui/material/IconButton";
-import DeleteIcon from "@mui/icons-material/Delete";
 import ListItemText from "@mui/material/ListItemText";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
+import TableContainer from "@mui/material/TableContainer";
 
 export default function Campaign({ data }) {
   const campaign = data;
@@ -46,51 +45,82 @@ export default function Campaign({ data }) {
         <Typography className={styles.title} variant="h6" component="div">
           Charities Selected
         </Typography>
-        <List dense={true}>
+        {/* <List dense={true}>
           {campaign.charitiesChosenByDonor.map((charity) => (
             <ListItem key={charity.id}>
               <ListItemText primary={charity.name} />
             </ListItem>
           ))}
-        </List>
+        </List> */}
+        <TableContainer component={Paper}>
+          <Table className={styles.table} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                {/* <TableCell>ID</TableCell> */}
+                <TableCell align="center">Name</TableCell>
+                {/* <TableCell align="right">Description</TableCell> */}
+                {/* <TableCell align="right">Link</TableCell> */}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {campaign.charitiesChosenByDonor.map((charity) => (
+                <TableRow
+                  key={charity.id}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  {/* <TableCell component="th" scope="row">
+                  {charity.id}
+                </TableCell> */}
+                  <TableCell align="center">{charity.name}</TableCell>
+                  {/* <TableCell align="right">{charity.description}</TableCell> */}
+                  {/* <TableCell align="right">{charity.link}</TableCell> */}
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
         <Typography className={styles.title} variant="h6" component="div">
           Vouchers Status
         </Typography>
-        <Table aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>ID</TableCell>
-              <TableCell align="right">Status</TableCell>
-              <TableCell align="right">Charity Selected</TableCell>
-              <TableCell align="right">Amount Added</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {campaign.vouchers.map((voucher) => (
-              <TableRow
-                key={voucher.id}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                  {voucher.id}
-                </TableCell>
-                <TableCell align="right">
-                  {voucher.status == 0
-                    ? "Unredeemed"
-                    : voucher.status == 1
-                    ? "Redeemed"
-                    : "Expired"}
-                </TableCell>
-                <TableCell align="right">
-                  {voucher.charityId ? charityMappings[voucher.charityId] : "-"}
-                </TableCell>
-                <TableCell align="right">
-                  {voucher.amountAdded ? "$" + voucher.amountAdded : "$0"}
-                </TableCell>
+        <TableContainer component={Paper}>
+          <Table className={styles.table} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>ID</TableCell>
+                <TableCell align="right">Status</TableCell>
+                <TableCell align="right">Charity Selected</TableCell>
+                <TableCell align="right">Amount Added</TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+            </TableHead>
+            <TableBody>
+              {campaign.vouchers.map((voucher) => (
+                <TableRow
+                  key={voucher.id}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {voucher.id}
+                  </TableCell>
+                  <TableCell align="right">
+                    {voucher.status == 0
+                      ? "Unredeemed"
+                      : voucher.status == 1
+                      ? "Redeemed"
+                      : "Expired"}
+                  </TableCell>
+                  <TableCell align="right">
+                    {voucher.charityId
+                      ? charityMappings[voucher.charityId]
+                      : "-"}
+                  </TableCell>
+                  <TableCell align="right">
+                    {voucher.amountAdded ? "$" + voucher.amountAdded : "$0"}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </Paper>
     </div>
   );
